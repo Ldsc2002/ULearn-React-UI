e12bbf52e2834eae857e871a06527e3d700c4397
Carol Arevalo <carol.arevalo@ext.firstlight.ai>
2022-04-20 18:07:41 -0600

    wip

--- src/Pizarron.js
+++ src/Pizarron.js
@@ -1,82 +1,100 @@
 import React, { Component } from 'react';
-import './App.css';
-import ScreenContext from './ScreenContext';
-import btn_icon_598731 from './images/btn_icon_598731.png';
-import btn_icon_452997 from './images/btn_icon_452997.png';
+import {
+  Stickies
+} from './indexPizarron.js';
 
-// UI framework component imports
-import Button from 'muicss/lib/react/button';
+const mock = require('./mock');
 
-export default class Pizarron extends Component {
+export default class extends Component {
 
-  static contextType = ScreenContext;
-
-  // This component doesn't use any properties
+  static defaultProps = {
+  };
 
   constructor(props) {
     super(props);
-    
+
     this.state = {
+      notes: [],
+      showTape: false,
+      showOutput: false,
+      showTitle: true,
+      showFooter: true,
+      output: '',
+      colors: ['#FFFFFF'],
+      showCustomColors: false,
+      showMock: false
     };
+    this.toggleValue = this.toggleValue.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.fetchMock = this.fetchMock.bind(this);
   }
 
-  componentDidMount() {
+  toggleValue(e) {
+    this.setState({
+      [e.target.name]: !this.state[e.target.name]
+    });
   }
 
-  componentWillUnmount() {
+  onChange(notes) {
+    this.setState({
+      output: JSON.stringify(notes, null, 2),
+      notes
+    });
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  fetchMock() {
+    this.setState({
+      showMock: true
+    }, () => {
+      this.setState({
+        notes: mock.default
+      });
+    });
   }
 
   render() {
-    
-    const style_elBackgroundShape = {
-      background: 'rgba(255, 255, 255, 1.000)',
-     };
-    const style_elText = {
-      color: 'black',
-      textAlign: 'center',
-     };
-    
-    const style_elFabCopy = {
-      display: 'block',
-      textTransform: 'none',
-      color: '(null)',
-      textAlign: 'center',
-     };
-    
-    const style_elFab = {
-      display: 'block',
-      textTransform: 'none',
-      color: '(null)',
-      textAlign: 'center',
-     };
-    
+    let wrapperStyle = {};
+    if (this.state.showBound) {
+      wrapperStyle = {
+        height: '700px',
+        width: '700px',
+        background: 'rgba(0, 0, 0, 0.2)',
+        border: '2px solid #fff',
+        overflow: 'auto',
+        padding: '10px'
+      };
+    }
     return (
-      <div className="Pizarron">
-        <div className="background">
-          <div className="containerMinHeight elBackgroundShape" style={style_elBackgroundShape} />
-        </div>
-        
-        <div className="layoutFlow">
-          <div className="elText">
-            <div className="baseFont" style={style_elText}>
-              <div>{this.context.locStrings.pizarron_text_376514}</div>
+      <div>
+        <Stickies
+          notes={this.state.notes}
+          tape={this.state.showTape}
+          style={{ float: 'left' }}
+          colors={this.state.showCustomColors ? this.state.colors : undefined}
+          title={this.state.showTitle}
+          footer={this.state.showFooter}
+          onChange={this.onChange}
+          wrapperStyle={wrapperStyle}
+        />
+        <div className="config">
+          <form>
+            <input type="radio" name="showCustomColors" value="show_custom_colors" checked={this.state.showCustomColors} onClick={this.toggleValue} />Custom Colors
+            <input type="radio" name="showTape" value="show_tape" checked={this.state.showTape} onClick={this.toggleValue} />Show Tape
+            <input type="radio" name="showOutput" value="show_output" checked={this.state.showOutput} onClick={this.toggleValue} />Show Output
+            <input type="radio" name="showTitle" value="show_title" checked={this.state.showTitle} onClick={this.toggleValue} />Show Title
+            <input type="radio" name="showFooter" value="show_footer" checked={this.state.showFooter} onClick={this.toggleValue} />Show Footer
+            <input type="radio" name="showMock" value="show_mock" checked={this.state.showMock} onClick={this.fetchMock} />Load Mock Data
+          </form>
+          <div className="output" style={{ display: this.state.showOutput ? 'block' : 'none' }}>
+            <label className="note-header">Output JSON</label>
+            <div>
+              <pre>
+                {this.state.output}
+              </pre>
             </div>
           </div>
         </div>
-        
-        <div className="foreground">
-          <Button className="actionFont elFabCopy" style={style_elFabCopy}  variant="fab" color="accent" >
-            <img src={btn_icon_598731} alt="" style={{width: '50.000%', marginTop: '25.000%'}} />
-          </Button>
-          <Button className="actionFont elFab" style={style_elFab}  variant="fab" color="accent" >
-            <img src={btn_icon_452997} alt="" style={{width: '50.000%', marginTop: '25.000%'}} />
-          </Button>
-        </div>
       </div>
-    )
+    );
   }
-  
 }
