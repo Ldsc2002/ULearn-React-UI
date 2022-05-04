a08ade722f0f91ea1ba30fb2eadc3d7a1f36d96a
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-04 16:36:56 -0600

    Updates

---
+++ precache-manifest.a3cf1c333b8312797a3ecccd89a9c94f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "64560a0f2e0a27eab67a",
+    "url": "/ULearn/static/css/main.eed9bb7f.chunk.css"
+  },
+  {
+    "revision": "64560a0f2e0a27eab67a",
+    "url": "/ULearn/static/js/main.ffa3dc78.chunk.js"
+  },
+  {
+    "revision": "ecbd617494a075366b69",
+    "url": "/ULearn/static/js/runtime~main.d4b9f38a.js"
+  },
+  {
+    "revision": "4684a054f79ec2a7af6e",
+    "url": "/ULearn/static/js/2.4dd184a7.chunk.js"
+  },
+  {
+    "revision": "3812709cdb14f4035d35f3cf528d5dd7",
+    "url": "/ULearn/static/media/CrearCuentaScreen_elCitCopy_837553.3812709c.jpg"
+  },
+  {
+    "revision": "9789115df2420302db52c45ef052aad4",
+    "url": "/ULearn/static/media/CrearCuentaScreen_elPerson_405468.9789115d.png"
+  },
+  {
+    "revision": "79adfa95862ea69779772842baaeb4b2",
+    "url": "/ULearn/static/media/studentUlearn.79adfa95.jpeg"
+  },
+  {
+    "revision": "5e91c1e8b2957161fa681b9148099117",
+    "url": "/ULearn/index.html"
+  }
+];
--- src/ScreenContext.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// This context is filled out by App when navigating into a screen.
-const ScreenContext = React.createContext({
-  appActions: {},
-  deviceInfo: {},
-});
-export default ScreenContext;
