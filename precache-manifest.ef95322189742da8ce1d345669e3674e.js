dd76e593aff0c7d543b1203bb207d7cdda9bc8e3
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-04 16:42:24 -0600

    Updates

---
+++ precache-manifest.ef95322189742da8ce1d345669e3674e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "24000eebc811bd3a238b",
+    "url": "/ULearn-React-UI/static/css/main.eed9bb7f.chunk.css"
+  },
+  {
+    "revision": "24000eebc811bd3a238b",
+    "url": "/ULearn-React-UI/static/js/main.ea53c929.chunk.js"
+  },
+  {
+    "revision": "904415eb4e6b8f06ef61",
+    "url": "/ULearn-React-UI/static/js/runtime~main.94079890.js"
+  },
+  {
+    "revision": "93ad30bc1f22b360bd54",
+    "url": "/ULearn-React-UI/static/js/2.003c333d.chunk.js"
+  },
+  {
+    "revision": "3812709cdb14f4035d35f3cf528d5dd7",
+    "url": "/ULearn-React-UI/static/media/CrearCuentaScreen_elCitCopy_837553.3812709c.jpg"
+  },
+  {
+    "revision": "9789115df2420302db52c45ef052aad4",
+    "url": "/ULearn-React-UI/static/media/CrearCuentaScreen_elPerson_405468.9789115d.png"
+  },
+  {
+    "revision": "79adfa95862ea69779772842baaeb4b2",
+    "url": "/ULearn-React-UI/static/media/studentUlearn.79adfa95.jpeg"
+  },
+  {
+    "revision": "471ef1bf79b700c1ee215740cb0f13f2",
+    "url": "/ULearn-React-UI/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ULearn/precache-manifest.b49f7852c685735b6f858c2b0b0e1061.js"
+  "/ULearn-React-UI/precache-manifest.ef95322189742da8ce1d345669e3674e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ULearn/index.html", {
+workbox.routing.registerNavigationRoute("/ULearn-React-UI/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
