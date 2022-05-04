e4f5bb2210912ff66a2bca27eeac226a9e818652
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-03 22:42:42 -0600

    UI fixes

--- src/book.js
+++ src/book.js
@@ -1,5 +1,6 @@
 import React from "react";
-import redBook from "./images/redbook.png";
+import "./book.css";
+
 
 const WidthProvider = require('react-grid-layout').WidthProvider;
 let ResponsiveReactGridLayout = require('react-grid-layout').Responsive;
@@ -7,19 +8,16 @@ let ResponsiveReactGridLayout = require('react-grid-layout').Responsive;
 ResponsiveReactGridLayout = WidthProvider(ResponsiveReactGridLayout);
 
 function BookCard(props) {
-    const style_book = {
-        backgroundImage: 'url('+redBook+')'
-    }
 
     const content = props.books.map((post) =>    
-        <div key={post.id}  style={style_book}>      
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
+        <div key={post.id} className="book-item">      
+            <h2 className="title">{post.title}</h2>
+            <p className="desc">{post.content}</p>
         </div>
     );
   
     return (
-        <ResponsiveReactGridLayout cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}>
+        <ResponsiveReactGridLayout>
             {content}
         </ResponsiveReactGridLayout>  
     );
--- src/bookShelf.js
+++ src/bookShelf.js
@@ -6,7 +6,6 @@ function Bookshelf() {
         {id: 1, title: 'Libro 1', content: 'Hello World'},
         {id: 2, title: 'Libro 2', content: 'Test'}
     ];
-    
 
     return (
         <div>
