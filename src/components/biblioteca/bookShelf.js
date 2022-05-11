aedcf0e12e052ea0c582a33d99fee0daba2245f7
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-11 17:41:42 -0600

    Added new pop ups

--- src/components/biblioteca/bookShelf.js
+++ src/components/biblioteca/bookShelf.js
@@ -5,6 +5,7 @@ import PopUp from "../popup/PopUp";
 
 function Bookshelf(props) {
     const [buttonPopUp, setButton] = useState(false);
+    const [showAdd, setShowAdd] = useState(false)
     const books = [
         {id: 1, title: 'Libro 1', content: 'Hello World'},
         {id: 2, title: 'Libro 2', content: 'Test'}
@@ -14,8 +15,26 @@ function Bookshelf(props) {
         <div>
             <BookCard books={books} setButton={setButton}/>
 
-            <PopUp trigger={buttonPopUp} setTrigger={setButton} onClick={() => props.setButton(false)}>
-                <h1>This is a test</h1>
+            <PopUp trigger={buttonPopUp} setTrigger={setButton} onClick={() => props.setButton(false)} >
+                <div>
+                    <h1>This is a title</h1>
+                    <p>This is a description</p>
+                    <button className="popUp-btn">Descargar</button>
+                    
+                </div>
+            </PopUp>
+
+            <div className='botonAgregarDiv'>
+                <button className='botonAgregar' onClick={() => setShowAdd(true)}>+</button>
+            </div>
+
+            <PopUp trigger={showAdd} setTrigger={setShowAdd} onClick={() => props.setShowAdd(false)} >
+                <div className="addPopUp">
+                    <input type="text" placeholder="Título"></input>
+                    <input type="text" placeholder="Descripción"></input>
+                    <input type="file"></input> 
+                    <button className="popUp-btn">Subir archivo</button>
+                </div>
             </PopUp>
         </div>
     );
