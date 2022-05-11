9eaecf64e7d3d44e26ecfedd08cbbc8c9fed04e5
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-10 21:37:47 -0600

    Removed unused files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import App from './components/app/App';
-import * as serviceWorker from './serviceWorker';
 
 import './style/app.css'
 import './style/book.css'
@@ -16,8 +15,4 @@ import './style/styles.css'
 
 
 ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
 
