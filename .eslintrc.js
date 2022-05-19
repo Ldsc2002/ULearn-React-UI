549b0ea938d0daecc9d82495a42d3c7fda0c5b26
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-19 14:54:44 -0600

    Set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+      browser: true,
+    },
+    extends: [
+      'plugin:react/recommended',
+      'airbnb',
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      },
+      ecmaVersion: 'latest',
+    },
+    plugins: [
+      'react',
+    ],
+    rules: {
+      "semi": [1, "never"],
+      "indent": ["error", 4],
+      "react/jsx-indent" : ["error", 4],
+      "react/jsx-indent-props" : ["error", 4],
+      "react/prop-types": "off"
+    }
+    ,
+  };
+  
