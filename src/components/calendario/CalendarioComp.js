cdb742bd18cc3e17f49048fc6e9bd200dd1f58d8
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-06-04 21:49:42 -0600

    guid

--- src/components/calendario/CalendarioComp.js
+++ src/components/calendario/CalendarioComp.js
@@ -18,11 +18,24 @@ function CalendarioComp() {
     const [mesS, setMes] = useState('')
     const [anoS, setAno] = useState('')
     const [contenidoS, setContenido] = useState('')
+    function guid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1)
+        }
+        return `${s4() + s4()}-${s4()}-${s4()}-${
+            s4()}-${s4()}${s4()}${s4()}`
+    }
 
     const newDateInador = () => {
         const fecha = `${diaS}-${mesS}-${anoS}`
 
-        db.collection('eventos').add({
+        console.log(tituloS, ' ', fecha, ' ', contenidoS)
+
+        const id = guid()
+
+        db.collection('eventos').doc(id).set({
             contenido: contenidoS,
             fecha,
             titulo: tituloS,
