0dfafa2d1c7e5c36736c53729e9eb4397aa385f5
Ldsc2002 <luissantoscuellar@gmail.com>
2022-08-07 21:24:28 -0600

    Refactor into functional component

--- src/components/calendario/Calendario.js
+++ src/components/calendario/Calendario.js
@@ -1,27 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ScreenContext from '../app/ScreenContext'
 import CalendarioComp from './CalendarioComp'
 
-// UI framework component imports
-
-export default class Calendario extends Component {
-    // This component doesn't use any properties
-    static contextType = ScreenContext;
-
-    constructor(props) {
-        super(props)
-
-        this.state = {
-        }
-    }
-
-    render() {
-        return (
-            <div className="Calendario">
-                <div className="container has-text-centered">
-                    <CalendarioComp />
-                </div>
+function calendario() {
+    return (
+        <div className="Calendario">
+            <div className="container has-text-centered">
+                <CalendarioComp />
             </div>
-        )
-    }
+        </div>
+    )    
 }
+
+export default calendario
