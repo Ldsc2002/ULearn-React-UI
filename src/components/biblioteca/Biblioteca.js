a41781f27a2d229ae8a8581ba098bad423609982
Ldsc2002 <luissantoscuellar@gmail.com>
2022-08-09 20:00:50 -0600

    Code cleanup

--- src/components/biblioteca/Biblioteca.js
+++ src/components/biblioteca/Biblioteca.js
@@ -1,17 +1,7 @@
 import React, { Component } from 'react'
 import Bookshelf from './bookShelf'
-import ScreenContext from '../app/ScreenContext'
 
 export default class Biblioteca extends Component {
-    static contextType = ScreenContext;
-
-    constructor(props) {
-        super(props)
-
-        this.state = {
-        }
-    }
-
     render() {
         return (
             <div>
