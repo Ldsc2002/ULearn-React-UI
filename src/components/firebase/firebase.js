c7cef7fb7f2f3ba2a649f1a60c9f73698dd47a3b
Ldsc2002 <luissantoscuellar@gmail.com>
2022-08-21 10:12:15 -0600

    Updated dependencies

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,17 +1,16 @@
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/firestore'
 import 'firebase/compat/auth'
-import 'dotenv/config'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: 'AIzaSyDtufOv6ScvHdl-SGog3CQmdEhtjl8oX6Q', 
+    authDomain: 'ulearn-42d0d.firebaseapp.com',
+    databaseURL: 'https://ulearn-42d0d-default-rtdb.firebaseio.com/',
+    projectId: 'ulearn-42d0d',
+    storageBucket: 'ulearn-42d0d.appspot.com', 
+    messagingSenderId: '897319107558',
+    appId: '1:1:897319107558:web:ba8d79e49bbd752e0ef427'
 }
 
 let app = ''
