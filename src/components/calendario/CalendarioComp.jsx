f99a9063e927ad14619cbaf3461e512cf2077e2a
Ldsc2002 <luissantoscuellar@gmail.com>
2022-08-27 19:26:02 -0600

    Better function names

--- src/components/calendario/CalendarioComp.jsx
+++ src/components/calendario/CalendarioComp.jsx
@@ -30,7 +30,7 @@ function CalendarioComp() {
             s4()}-${s4()}${s4()}${s4()}`
     }
 
-    const newDateInador = () => {
+    const addEventHandler = () => {
         const fechas = `${day}-${month}-${year}`
 
         console.log(title, ' ', fechas, ' ', content)
@@ -49,7 +49,7 @@ function CalendarioComp() {
         setAddDate(false)
     }
 
-    const readInador = (e) => {
+    const inputDataHandler = (e) => {
         if (e.target.name === 'titulo') {
             setTitle(e.target.value)
         } else if (e.target.name === 'dia') {
@@ -63,7 +63,7 @@ function CalendarioComp() {
         }
     }
 
-    const borraInador = () => {
+    const eraseDataHandler = () => {
         const { id } = event
 
         db.collection('eventos').doc(id).delete()
@@ -119,6 +119,10 @@ function CalendarioComp() {
                     let month = date.split("-")[1]
                     let year = date.split("-")[2]
 
+                    setDay(day)
+                    setMonth(month)
+                    setYear(year)
+
                     document.getElementsByName('dia')[0].value = day
                     document.getElementsByName('mes')[0].value = month
                     document.getElementsByName('ano')[0].value = year
@@ -173,32 +177,32 @@ function CalendarioComp() {
                     <div className="fechaInador1">
                         <div className='ingresador'>
                             <h3>TITULO</h3>
-                            <input type="text" name="titulo" onChange={readInador} />
+                            <input type="text" name="titulo" onChange={inputDataHandler} />
                         </div>
                     </div>
                     
                     <div className="fechaInador2">
                         <div className='ingresador'>
                             <h3>DIA</h3>
-                            <input name="dia" type="text" onChange={readInador} />
+                            <input name="dia" type="text" onChange={inputDataHandler} />
                         </div>
                         <div className='ingresador'>
                             <h3>MES</h3>
-                            <input name="mes" type="text" onChange={readInador} />
+                            <input name="mes" type="text" onChange={inputDataHandler} />
                         </div>
                         <div className='ingresador'>
                             <h3>AÑO</h3>
-                            <input name="ano" type="text" onChange={readInador} />
+                            <input name="ano" type="text" onChange={inputDataHandler} />
                         </div>
                     </div>
 
                     <div className="fechaInador3">
                         <div className='ingresador'>
                             <h3>CONTENIDO</h3>
-                            <input name="contenido" type="text" onChange={readInador} />
+                            <input name="contenido" type="text" onChange={inputDataHandler} />
                         </div>
                     </div>
-                    <button className='continuaBotoncito' type="button" onClick={newDateInador}>Continuar</button>
+                    <button className='continuaBotoncito' type="button" onClick={addEventHandler}>Continuar</button>
 
                 </div>
             </PopUp>
@@ -213,7 +217,7 @@ function CalendarioComp() {
                     <div className='eventDiv2'>
                         <h2>Información: {event.contenido}</h2>
                     </div>
-                    <button type="button" className="buttonControl" onClick={borraInador}>BORRAR</button> 
+                    <button type="button" className="buttonControl" onClick={eraseDataHandler}>BORRAR</button> 
                 </div>
             </PopUp>
 
