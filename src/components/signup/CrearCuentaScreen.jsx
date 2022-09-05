80d61c3b9f850973c61860590fa4e4f9a398e605
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2022-09-04 22:22:12 -0600

    spanish message

--- src/components/signup/CrearCuentaScreen.jsx
+++ src/components/signup/CrearCuentaScreen.jsx
@@ -96,8 +96,29 @@ export default class CrearCuentaScreen extends Component {
 
             this.context.appActions.goToScreen('start', this.context.baseScreenId, { transitionId: 'fadeIn' })
         } catch (error) {
-            console.error(error);
-            alert(error.message);
+            if(error.code == 'auth/user-not-found') {
+                alert('Usario no existe.\nPor favor cree una cuenta.')
+            } else if(error.code == 'auth/wrong-password') {
+                alert('Contraseña incorrecta.\nIntente de nuevo.')
+            } else if( error.code == 'auth/email-already-in-use') {
+                alert("Por favor utilice otro correo.\nEste correo ya esta en uso. Si error continua, contacte a servicio tecnico", [
+                    {
+                      text: "Ok",
+                      onPress: () => null,
+                      style: "cancel",
+                    },
+                    {
+                      text: "Contactar a servicio tecnico",
+                      onPress: () => Linking.openURL('mailto: ara20261@yvg.edu.gt'),
+                    },
+                  ]);
+            } else if(error.code == 'auth/network-request-failed') {
+                alert('Error de red.\nPor favor intente nuevamente o revise su conexión a internet.')
+            } else if(error.code == 'auth/invalid-email') {
+                alert('Correo invalido.\nPor favor revise que haya ingresado un correo valido.')
+            } else {
+                alert('Error desconocido.\nIntente más tarde.')
+            }  
         }
     }
 
