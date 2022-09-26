d0194dae9f89c9bb075786c61c34d3d7ee3cffa5
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-09-26 15:05:34 -0600

    last

--- tests/CalendarioScreen.test.js
+++ tests/CalendarioScreen.test.js
@@ -2,35 +2,20 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import Calendario from '../src/components/calendario/CalendarioComp';
 
-jest.mock('firebase/auth', () => {
+//firebase testing
+jest.mock('../src/components/firebase/firebase', () => {
   return {
-    setPersistence: jest.fn(),
-    signInWithEmailAndPassword: jest.fn(),
-    browserLocalPersistence: jest.fn(),
+      auth: jest.fn(),
+      db: jest.fn(),
   };
 });
 
-jest.mock('../src/components/firebase/firebase', () => {
-    return {
-        auth: jest.fn(),
-        db: jest.fn(),
-    };
-});
+//test to check if the Pizarron component renders correctly
+test('calendario_render_test', () => {
 
-
-// This test is to check if the Calendario component renders correctly
-test('renders LogInScreen', () => {
-    const props = {email: 'stefano@uvg.edu.gt'};
-    render(<Calendario {...props} />);
-});
-
-// test onClick_elButton to login
-test('dateClickHandler', () => {
-    window.alert = jest.fn();
-
-    Calendario.dateClickHandler('4-9-2022');
-
-    expect(loginScreen.event.titulo).toBe("hoy");
-    expect(loginScreen.event.fecha).toBe("4-9-2022");
-    expect(loginScreen.event.contenido).toBe("comer");
+   const props = {
+      email: 'stefano@uvg.edu.gt'
+  }
+  const calendario = new Calendario(props);
+  expect(calendario).not.toBeNull();
 });
