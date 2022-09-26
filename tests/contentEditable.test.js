59c65ed186a2e45e88c94708381ef3fb434554bf
CA-WotDev <are20461@uvg.edu.gt>
2022-09-25 19:53:24 -0600

    Create contentEditable.test.js

---
+++ tests/contentEditable.test.js
@@ -0,0 +1,72 @@
+import {render, screen} from '@testing-library/react'
+import { EditorState } from 'draft-js';
+import React from 'react';
+import { db } from '../src/components/firebase/firebase';
+import ContentEditable from '../src/components/pizarron/ContentEditable';
+
+//firebase testing
+jest.mock('../src/components/firebase/firebase', () => {
+    return {
+        auth: jest.fn(),
+        db: jest.fn(),
+    };
+});
+
+//test to check if the ContentEditable component renders correctly
+test('renders ContentEditable', () => {
+    render(<ContentEditable />);
+});
+
+//test shouldComponentUpdate
+test('shouldComponentUpdate', () => {
+    const props = {
+        html: "hola",
+        disabled: false,
+        onChange: jest.fn(),
+    }
+
+    
+    const contentEditable = new ContentEditable(props);
+    contentEditable.editable = {
+        innerHTML: "hola",
+    }
+
+    const shouldUpdate = contentEditable.shouldComponentUpdate(props);
+    expect(shouldUpdate).toBe(false);
+});
+
+//test componentDidUpdate
+test('componentDidUpdate', () => {
+    const props = {
+        html: "hola",
+        disabled: false,
+        onChange: jest.fn(),
+    }
+
+    
+    const contentEditable = new ContentEditable(props);
+    contentEditable.editable = {
+        innerHTML: "hola",
+    }
+
+    contentEditable.componentDidUpdate(props);
+    expect(contentEditable.editable.innerHTML).toBe("hola");
+});
+
+//test emitChange to call props.onChange
+test('emitChange', () => {
+    const props = {
+        html: "hola",
+        disabled: false,
+        onChange: jest.fn(),
+    }
+
+    
+    const contentEditable = new ContentEditable(props);
+    contentEditable.editable = {
+        innerHTML: "hola",
+    }
+
+    contentEditable.emitChange();
+    expect(props.onChange).toHaveBeenCalled();
+});
