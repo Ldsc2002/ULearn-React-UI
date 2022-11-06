dc415e1c0520e82484ee56aba94a0cdbc573a0cf
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-11-05 19:51:04 -0600

    add get prev and next month

--- tests/CalendarioScreen.test.js
+++ tests/CalendarioScreen.test.js
@@ -51,45 +51,16 @@ test('renders_calendario_screen_test', () => {
   render(<Calendario />);
 });
 
-// //test date click handler
-// test('date_click_handler', () => {
- 
-//   const props = {
-//     email: 'stefano@uvg.edu.gt'
-//   }
-//   const calendario = new Calendario(props);
+//test getprevmonth
+test('get_prev_month_test', () => {
+  let calendario = new CalendarioComp();
+  calendario.getPrevMonth();
+  expect(calendario.state.month).toBe(11);
+});
 
-//   date = '4-9-2022';
-//   const dateClickHandler = calendario.dateClickHandler(date);
-//   expect(dateClickHandler).not.toBeNull();
-// });
-
-// // //test readInador
-// test('new_date_inador', () => {
-//   const props = {
-//     email: 'stefano@uvg.edu.gt'
-//   }
-//   const calendario = new Calendario(props);
-
-//   const content = 'test';
-//   const fecha = '1-9-2022';
-//   const title = 'test';
-//   const user = 'stefano@uvg.edu.gt';
-//   const hoyEs = '1-9-2022';
-
-//   const newDateI = calendario.borrarInador(content, fecha, title, user, hoyEs);
-//   expect(newDateI).not.toBeNull();
-// });
-
-// //test nameClass
-// test('name_class', () => {
-//   const props = {
-//     email: 'stefano@uvg.edu.gt'
-//   }
-//   const calendario = new Calendario(props);
-
-//   const fecha = '1-9-2022';
-
-//   const nameC = calendario.nameClass(fecha);
-//   expect(nameC).not.toBeNull();
-// });
+//test getnextmonth
+test('get_next_month_test', () => {
+  let calendario = new CalendarioComp();
+  calendario.getNextMonth();
+  expect(calendario.state.month).toBe(1);
+});
