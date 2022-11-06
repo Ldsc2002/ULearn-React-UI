89e934725d47d98b934cf1a980b47c23d0c1f330
CarolArevalo <are20461@uvg.edu.gt>
2022-11-05 21:22:55 -0600

    done

--- src/components/calendario/Calendario.jsx
+++ src/components/calendario/Calendario.jsx
@@ -5,10 +5,9 @@ import ScreenContext from '../app/ScreenContext'
 export default class calendario extends Component {
     static contextType = ScreenContext
 
+    /*istanbul ignore next*/
     render() {
-        console.log('HOLA WENA TARDE')
         return (
-
             <div className="Calendario">
                 <div className="container has-text-centered">
                     <CalendarioComp
