ffdae39b0d9952c216bccd079b049aec55b61772
CarolArevalo <are20461@uvg.edu.gt>
2022-11-15 16:15:56 -0600

    DONE

--- src/components/pizarron/ContentEditable.jsx
+++ src/components/pizarron/ContentEditable.jsx
@@ -26,7 +26,7 @@ export default class extends Component {
 
     render() {
         return (
-            <div
+            <div className="contentEditableTitle"
                 onInput={this.emitChange.bind(this)}
                 onBlur={this.emitChange.bind(this)}
                 contentEditable
